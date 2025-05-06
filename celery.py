b42af54937a9e94b8c04ef3ca908eec8e591ec38
Thiagoc.machado <thiagocmachado@yahoo.com.br>
2025-05-06 09:06:19 +0200

    redis

--- celery.py
+++ celery.py
@@ -2,7 +2,8 @@ from celery import Celery
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+# força o caminho do .env na raiz do container
+load_dotenv(dotenv_path='/app/.env')
 
 app = Celery('app', broker=os.getenv('CELERY_BROKER_URL'))
 app.autodiscover_tasks(['app'])
