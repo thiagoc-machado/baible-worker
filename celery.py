5bb0535b8565e63df89f5334d697cfc5485d18bf
Thiagoc.machado <thiagocmachado@yahoo.com.br>
2025-05-06 08:57:30 +0200

    redis

--- celery.py
+++ celery.py
@@ -4,9 +4,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-app = Celery(
-    'app',
-    broker=os.getenv('CELERY_BROKER_URL'),
-)
-
+app = Celery('app', broker=os.getenv('CELERY_BROKER_URL'))
 app.autodiscover_tasks(['app'])
